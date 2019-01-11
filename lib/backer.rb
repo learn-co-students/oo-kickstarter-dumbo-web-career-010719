0f46f7709ac994dc0e969f056cd572db6967a2ef
ali-mohammed200 <ali.mohammed200@gmail.com>
2019-01-11 18:38:12 -0500

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,11 @@
+class Backer
+  attr_accessor :name, :backed_projects
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+  def back_project(proj)
+    @backed_projects << proj
+    proj.add_backer(self)
+  end
+end
